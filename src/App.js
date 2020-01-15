7f276e4f6fc3ead4111f73ace79131ae1ae08626
gutovesco <guto.vesco@hotmail.com>
2020-01-15 17:15:59 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './global.css';
 import './App.css';
+import './sidebar.css';
+
 //Componente = funcao q retorna um conteudo (HTML, CSS, JS), primeira letra sempre maiuscula, um por arquivo
 //Propriedade = informaçoes pássadas por hierarquia, por exemplo: src, title, class
 //Estado = informaooes mantidas pelo componente(imutabilidade(nao alterar dados, criar um novo e substituir))
@@ -10,11 +12,37 @@ function App() {
   return(
     <div id="app">
       <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+          <label htmlFor="github_username">Usuário do Github</label>
+          <input name="github_username" id="github_username" required/>
+          </div>
 
+          <div class="input-block">
+          <label htmlFor="techs">Tecnologias</label>
+          <input name="techs" id="techs" required/>
+          </div>
 
+        <div className="input-group">
+
+          <div class="input-block">
+            <label htmlFor="latitude">Latitude</label>
+            <input name="latitude" id="latitude" required/>
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="=longitude">Longitude</label>
+            <input name="longitude" id="longitude" required/>
+          </div>
+        </div>
+         <button type="submit">Salvar</button>
+        </form>
       </aside>
       <main>
-
+        <ul>
+          
+        </ul>
       </main>
 
     </div>
