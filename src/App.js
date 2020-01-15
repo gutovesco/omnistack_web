fc274e7b97bac86e004d1200999a4523b24a21d3
gutovesco <guto.vesco@hotmail.com>
2020-01-15 16:51:09 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import './global.css';
+import './App.css';
 //Componente = funcao q retorna um conteudo (HTML, CSS, JS), primeira letra sempre maiuscula, um por arquivo
 //Propriedade = informaçoes pássadas por hierarquia, por exemplo: src, title, class
 //Estado = informaooes mantidas pelo componente(imutabilidade(nao alterar dados, criar um novo e substituir))
@@ -13,7 +14,7 @@ function App() {
 
       </aside>
       <main>
-        
+
       </main>
 
     </div>
